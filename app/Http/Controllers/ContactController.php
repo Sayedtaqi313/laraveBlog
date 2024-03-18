b7a53d65e266adf6fc7deee62c2b5233064e1431
sayed taqi <sayed@gmail.com>
2024-03-18 01:55:04 -0700

    finishing the email

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactRequest;
+use App\Mail\ContactUs;
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -14,7 +16,7 @@ class ContactController extends Controller
     }
 
     public function store(ContactRequest $request) {
-        $contact = Contact::create([
+       $data =  $contact = Contact::create([
             'firstName' => $request->firstName,
             'lastName' => $request->lastName,
             'email' => $request->email,
@@ -25,6 +27,7 @@ class ContactController extends Controller
         if(!$contact) {
             return back()->with('eorror','there is a problem please try again');
         }
+            Mail::to('sayedtaqi313ahmadi@gmail.com')->send(new ContactUs($data));
             return back()->with('success','you message have been sent successfully');
     }
 }
---
+++ resources/views/email/contact.blade.php
@@ -0,0 +1,12 @@
+<x-mail::message>
+# Introduction
+
+{{ $data['message'] }}
+
+<x-mail::button :url="''">
+Button Text
+</x-mail::button>
+
+Thanks,<br>
+
+</x-mail::message>
